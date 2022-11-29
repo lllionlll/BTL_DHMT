f4034f153a84d5ed500f9ad1ad8766de32e03554
lllionlll <lam14.dev@gmail.com>
2022-11-30 01:43:21 +0700

    feat: code mohinh_ghe

--- GLSLExperiment/example1.cpp
+++ GLSLExperiment/example1.cpp
@@ -26,7 +26,10 @@ point4 vertices[8]; /* Danh sách 8 đỉnh của hình lập phương*/
 color4 vertex_colors[8]; /*Danh sách các màu tương ứng cho 8 đỉnh hình lập phương*/
 
 GLuint program;
-
+GLuint model_loc;
+mat4 model;
+mat4 instance;
+mat4 transl;
 void initCube()
 {
 	// Gán giá trị tọa độ vị trí cho các đỉnh của hình lập phương
@@ -109,16 +112,68 @@ void shaderSetup( void )
 	GLuint loc_vColor = glGetAttribLocation(program, "vColor");
 	glEnableVertexAttribArray(loc_vColor);
 	glVertexAttribPointer(loc_vColor, 4, GL_FLOAT, GL_FALSE, 0, BUFFER_OFFSET(sizeof(points)));
-
+	model_loc = glGetUniformLocation(program, "Model");
+	glEnable(GL_DEPTH_TEST);
     glClearColor( 1.0, 1.0, 1.0, 1.0 );        /* Thiết lập màu trắng là màu xóa màn hình*/
 }
+mat4 quay_tua_lung;
+mat4 quay_ghe;
+mat4 nang_ghe;
+mat4 di_chuyen_ghe;
+GLfloat theta[16];
+void Ghe(GLfloat TranX, GLfloat TranY, GLfloat TranZ, GLfloat width, GLfloat height, GLfloat depth)
+{
+	mat4 instance = Translate(TranX, TranY, TranZ) * quay_tua_lung * Scale(width, height, depth);
 
+	glUniformMatrix4fv(model_loc, 1, GL_TRUE, model * transl * di_chuyen_ghe * nang_ghe * quay_ghe * instance);
+	glDrawArrays(GL_TRIANGLES, 0, NumPoints);
+}
+
+GLfloat de_ghe_x = 0.1f, de_ghe_y = 0.015f, de_ghe_z = 0.1f;
+GLfloat chan_ghe_x = 0.015f, chan_ghe_y = 0.1f, chan_ghe_z = 0.015f;
+
+GLfloat than_de_x = 0.15f, than_de_y = 0.015f, than_de_z = 0.12f;
+GLfloat tua_lung_x = 0.15f, tua_lung_y = 0.2f, tua_lung_z = 0.015f;
+GLfloat tua_tay_x = 0.02f, tua_tay_y = 0.02f, tua_tay_z = 0.1f;
+void Build_Ghe()
+{
+	GLfloat x = 0.0f, y = 0.5 * de_ghe_y, z = 0.0f;
+	di_chuyen_ghe = Translate(theta[9] + theta[10], 0.0f, theta[11] + theta[12]); //  cái theta trong keyboard này
+	// đế ghế (đáy ghế) -> cha
+	Ghe(x, y, z, de_ghe_x, de_ghe_y, de_ghe_z);
+
+	// chân ghế  -> con của thằng đáy ghế
+	x = 0.0f, y = de_ghe_y + 0.5 * chan_ghe_y, z = 0.0f;
+	Ghe(x, y, z, chan_ghe_x, chan_ghe_y, chan_ghe_z);
+
+	// code quay ghế 
+	quay_ghe = Translate(0.0f, (de_ghe_y + chan_ghe_y + 0.5 * than_de_y), 0.0f)
+		* RotateY(theta[7]) * RotateY(theta[8]) * Translate(0.0f, -(de_ghe_y + chan_ghe_y + 0.5 * than_de_y), 0.0f);
+
+	// đế ngồi --> con của thằng chân ghế, cháu của đáy ghế
+	x = 0.0f, y = de_ghe_y + chan_ghe_y + 0.5 * than_de_y, z = 0.0f;
+	Ghe(x, y, z, than_de_x, than_de_y, than_de_z);
+
+	// tựa lưng  -> con của thằng chân ghế, cháu của đáy ghế
+	quay_tua_lung = Translate(0.0f, -0.5 * tua_lung_y, 0.0f) * RotateX(20) * Translate(0.0f, 0.5 * tua_lung_y, 0.0f);
+	x = 0.0f, y = de_ghe_y + chan_ghe_y + 0.5 * than_de_y + 0.5 * tua_lung_y, z = 0.5 * de_ghe_z;
+	Ghe(x, y, z, tua_lung_x, tua_lung_y, tua_lung_z);
+	quay_tua_lung = identity();
+
+	// tựa tay -->con của thằng chân ghế, cháu của đáy ghế
+	x = 0.5 * tua_lung_x - 0.5 * tua_tay_x, y = de_ghe_y + chan_ghe_y + than_de_y + 0.3 * tua_lung_y, z = tan(M_PI / 9) * (0.3 * tua_lung_y);
+	Ghe(x, y, z, tua_tay_x, tua_tay_y, tua_tay_z);
+
+	Ghe(-x, y, z, tua_tay_x, tua_tay_y, tua_tay_z);
+	quay_ghe = identity();
+	nang_ghe = identity();
+}
 
 void display( void )
 {
 	
     glClear( GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT );                
-    glDrawArrays( GL_TRIANGLES, 0, NumPoints );    /*Vẽ các tam giác*/
+	Build_Ghe();
 	glutSwapBuffers();									   
 }
 
@@ -131,7 +186,28 @@ void keyboard( unsigned char key, int x, int y )
     case 033:			// 033 is Escape key octal value
         exit(1);		// quit program
         break;
+	case 'q':
+		//di chuyển sang trái
+		theta[9] -= 0.05f;
+		glutPostRedisplay();
+		break;
+	case 'w':
+		//di chuyển sang phải
+		theta[10] += 0.05f;
+		glutPostRedisplay();
+		break;
+	case 'e':
+		//di chuyển về phía trước
+		theta[11] -= 0.05f;
+		glutPostRedisplay();
+		break;
+	case 'r':
+		//di chuyển ra sau
+		theta[12] += 0.05f;
+		glutPostRedisplay();
+		break;
     }
+	
 }
 
 
--- GLSLExperiment/vshader1.glsl
+++ GLSLExperiment/vshader1.glsl
@@ -2,9 +2,9 @@
 in vec4 vPosition;
 in vec4 vColor;
 out vec4 color;
-
+uniform mat4 Model;
 void main()
 {
-    gl_Position = vPosition;
+    gl_Position =Model*vPosition;
 	color=vColor;
 }//
